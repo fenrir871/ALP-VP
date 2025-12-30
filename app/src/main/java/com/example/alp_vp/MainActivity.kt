6db2cad20050c505547fd26065d9e3dd1b223d6e
Kenn <keanejuan07@gmail.com>
2025-12-30 14:19:31 +0700

    App Navigation

--- app/src/main/java/com/example/alp_vp/MainActivity.kt
+++ app/src/main/java/com/example/alp_vp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.example.alp_vp.data.api.RetrofitClient
 import com.example.alp_vp.data.local.TokenManager
+import com.example.alp_vp.ui.route.AppNavigation
 import com.example.alp_vp.ui.theme.ALPVPTheme
 import com.example.alp_vp.ui.view.Friend.Friend
 import kotlinx.coroutines.launch
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 
             setContent {
                 ALPVPTheme {
-                    Friend()
+                    AppNavigation()
                 }
             }
         }
